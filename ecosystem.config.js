296bd5e7c68bc5c053f48fa55bde73310439c855
souravhashtag <souravg.hashtag@gmail.com>
2025-12-11 19:30:02 +0530

    chore: update port to 3000 in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
             exec_mode: 'cluster',
             env: {
                 NODE_ENV: 'development',
-                PORT: 5000,
+                PORT: 3000,
             },
             env_production: {
                 NODE_ENV: 'production',
-                PORT: 5000,
+                PORT: 3000,
             },
             error_file: 'logs/pm2-error.log',
             out_file: 'logs/pm2-out.log',
